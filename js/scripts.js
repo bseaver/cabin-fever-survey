7673bb56c6ec12a4d7441d2ecca73e07dc6dd1e0
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-01-17 17:27:01 -0800

    add ability to present output on submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
+  // Message if not checkboxes apply
+  var unaffectedMessage = "Wow! You are unaffected by snow-mageddon!";
+
   // [category, categoryQuestion, categoryConclusion]
   var categories = [
-    ["study", "How has snow-mageddon affected your studies? (Check all that apply)", "Snow-mageddon has affected your studies: "],
-    ["home", "How has snow-mageddon affected your home / personal life? (Check all that apply)", "Snow-mageddon has affected your personal life: "]
+    ["study", "How has snow-mageddon affected your studies? (Check all that apply)", "Snow-mageddon has affected your studies:"],
+    ["home", "How has snow-mageddon affected your home / personal life? (Check all that apply)", "Snow-mageddon has affected your personal life:"]
   ];
 
   // [[categories], checkboxValue, checkboxQuestion, checkboxConclusion]
@@ -52,10 +55,48 @@ $(document).ready(function(){
   // Finally append submit button
   $("#inputForm").append(buttonTemplate);
 
+  ////////////////////////////////////////////////////
+
   // Respond to submit button
   $("#inputForm").submit(function(event) {
     event.preventDefault();
-    alert("submit");
-  });
+
+    var someCheckboxChecked = false;
+    var outputMessage = "";
+
+    categories.forEach(function(category) {
+      var categoryID = category[0];
+      var categoryConclusion = category[2];
+
+      // Which checkboxes were checked for this category?
+      $("#inputForm input:checkbox[name=" + categoryID +"]:checked").each(function() {
+        someCheckboxChecked = true;
+
+        var checkboxValue = $(this).val();
+
+        var checkboxIndex = checkboxes.findIndex(function(element) {
+          return element[1] === checkboxValue;
+        });
+
+        // Output the category conclusion just one time per category
+        if (categoryConclusion) {
+          if (outputMessage) {
+            outputMessage += "\n\n";
+          }
+          outputMessage += categoryConclusion;
+          categoryConclusion = false;
+        }
+
+        outputMessage += "  " + checkboxes[checkboxIndex][3];
+      }); // End which checkboxes were checked
+    }); // End categories.forEach
+
+    // Handle case where no checkboxes were checked
+    if (!someCheckboxChecked) {
+      outputMessage = unaffectedMessage;
+    }
+    $("#outputMessage").text(outputMessage);
+    $("#outputSection").fadeOut(100).fadeIn(100);
+  }); // End Respond to submit button
 
 }); // End Document Ready
